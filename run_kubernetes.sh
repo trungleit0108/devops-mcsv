2f14af6667197823892da519934836db8e6b3cda
Trung.MinhLe <trungleit0108@gmail.com>
2023-07-10 22:57:25 +0700

    task6

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=trungleit0108/image_demo
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run container-demo --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward container-demo 8080:80
 
