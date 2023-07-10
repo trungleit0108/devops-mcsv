11e3910ef34d04533f4905b35ab8186f6523741e
Trung.MinhLe <trungleit0108@gmail.com>
2023-07-10 22:17:20 +0700

    task4

--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 
-docker build --tag=latest .
+docker build --tag=image_demo .
 
 docker images
 
-docker run -p 8000:80 latest
+docker run -p 8000:80 image_demo
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=trungleit0108/image_demo
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag image_demo $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath:latest
